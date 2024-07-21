3a6afbdbe980c4ee4b31307745781ffcca2ef17b
Frank Mayer <gh@frankmayer.dev>
2024-07-21 19:43:11 +0200

    added exception

--- src/components/react/Code.tsx
+++ src/components/react/Code.tsx
@@ -1,10 +1,10 @@
 import type { ReactNode } from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
-import { mocha } from "./PrismCatppuccinTheme"
+import { catppuccin } from "./PrismCatppuccinTheme"
 
 export function Code(props: { code: string }): ReactNode {
     return (
-        <SyntaxHighlighter language="java" style={mocha} showLineNumbers>
+        <SyntaxHighlighter language="java" style={catppuccin()} showLineNumbers>
             {props.code}
         </SyntaxHighlighter>
     )
