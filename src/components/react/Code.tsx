4bcea94b52c67dcc5c9eceb236c06574e6a0de1a
Frank Mayer <gh@frankmayer.dev>
2024-08-02 14:32:20 +0200

    reduced layout shifting

--- src/components/react/Code.tsx
+++ src/components/react/Code.tsx
@@ -2,9 +2,9 @@ import type { ReactNode } from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { catppuccin } from "./PrismCatppuccinTheme"
 
-export function Code(props: { code: string }): ReactNode {
+export function Code(props: { code: string; language?: string | undefined }): ReactNode {
     return (
-        <SyntaxHighlighter language="java" style={catppuccin()} showLineNumbers>
+        <SyntaxHighlighter language={props.language || "java"} style={catppuccin()} showLineNumbers>
             {props.code}
         </SyntaxHighlighter>
     )
